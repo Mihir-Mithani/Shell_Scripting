4b22f981840810713d8af98b7a234fcc63a728b2
mihirmithani <mihirmithani123@gmail.com>
2026-06-26 16:07:34 +0530

    Logical Operator

---
+++ Logical_Operator/AND.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#Logical Operators && : and , || : or , -eq for numerical and == for string
+
+read -p "Enter the Age : " age
+read -p "Enter your Country : " country
+
+if [[ $age -ge 18 ]] && [[ $country == "India" ]]
+then
+    echo "You can vote"
+else
+    echo "You can't vote"
+fi
+
