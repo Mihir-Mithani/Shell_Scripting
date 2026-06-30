b172d4715fa49b672948ac8fbdca6cac1130e31d
mihirmithani <mihirmithani123@gmail.com>
2026-06-30 11:01:57 +0530

    For Loops

---
+++ Loops/For_Loop/for1.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+#For loops
+
+#format 1
+for i in 1 2 3 4 5 6 7 8 9 10
+do
+    echo "Number is : $i "
+done
+
+#format 2
+for j in Mihir Mithani
+do
+    echo "String is : $j"
+done
+
+for i in {1..5}
+do
+    echo "Number is : $i"
+done
