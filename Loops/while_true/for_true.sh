f6f37d35af74f2dc82b299ef2a7ec78e142008f6
mihirmithani <mihirmithani123@gmail.com>
2026-07-06 10:19:24 +0530

    infinite loops

---
+++ Loops/while_true/for_true.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#infinite loop
+
+for (( ;; ))
+do
+    echo "it is infinite"
+    sleep 2s
+done
+
+
---
+++ Loops/while_true/while_true.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#infinite loop
+
+while true
+do
+    echo "it is infinite"
+    sleep 2s
+done
+
+
