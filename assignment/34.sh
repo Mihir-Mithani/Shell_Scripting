6e00913901dc559461d9fee51a6401f836303d1c
mihirmithani <mihirmithani123@gmail.com>
2026-07-08 10:49:30 +0530

    Assignment

--- assignment/34.sh
+++ assignment/34.sh
@@ -1,3 +1,17 @@
 # Author : Mihir Mithani
 
 #!/opt/homebrew/bin/bash
+
+echo "--------------------"
+echo "-----Login Page-----"
+echo "--------------------"
+
+read -p "Enter Username : " username
+read -p "Enter password for $username : " password
+
+if [[ "$username"=="mihir" && "$password"=="12345678" ]];
+then
+    echo "Login Succesfully"
+else
+    echo "Invalid Credentials"
+fi
