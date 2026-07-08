512ad21718e56feb9adca755ca62567c5ffdc4db
mihirmithani <mihirmithani123@gmail.com>
2026-07-08 13:33:37 +0530

    Assignment

--- assignment/42.sh
+++ assignment/42.sh
@@ -1,3 +1,18 @@
 # Author : Mihir Mithani
 
 #!/opt/homebrew/bin/bash
+
+error_handler() {
+    echo "An error occurred!"
+    echo "Error on line $1"
+    echo "Exit status: $2"
+}
+
+trap 'error_handler $LINENO $?' ERR
+
+echo "Starting script..."
+
+echo "Dividing by zero..."
+result=$((10 / 0))
+
+echo "This line will not execute."
