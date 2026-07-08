424b01915154a73e772f62ea0b889d5cfb353cfa
mihirmithani <mihirmithani123@gmail.com>
2026-07-08 10:49:40 +0530

    Assignment

--- assignment/39.sh
+++ assignment/39.sh
@@ -1,3 +1,19 @@
 # Author : Mihir Mithani
 
 #!/opt/homebrew/bin/bash
+
+fact(){
+    local num=$1
+    local facto=1
+    for ((i=1;i<=num;i++))
+    do
+        facto=$((facto*i))
+    done
+
+    echo "Factorial of $num : $facto"
+
+}
+
+read -p "Enter The Number : " num
+
+fact $num
