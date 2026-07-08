1186a6ab9f4742e916cdd196579caa287a06ec3b
mihirmithani <mihirmithani123@gmail.com>
2026-07-08 10:49:26 +0530

    Assignment

--- assignment/32.sh
+++ assignment/32.sh
@@ -1,3 +1,31 @@
 # Author : Mihir Mithani
 
 #!/opt/homebrew/bin/bash
+
+
+calculate(){
+    local num1=$1
+    local num2=$2
+    local choice=$3
+    case "$choice" in
+        +)
+            echo "Answer : $((num1 + num2))"
+            ;;
+        -)
+            echo "Answer : $((num1 - num2))"
+            ;;
+        *)
+            echo "Answer : $((num1 * num2))"
+            ;;
+        /)
+            echo "Answer : $((num1 / num2))"
+            ;;
+    esac
+
+}
+read -p "Enter first number : " num1
+read -p "Enter second Number : " num2
+
+read -p "Enter choice +-*/ : " choice
+
+calculate $num1 $num2 $choice
