fec12a657310e009598e99a38c0b5398ed631321
mihirmithani <mihirmithani123@gmail.com>
2026-07-08 13:33:50 +0530

    Assignment

--- assignment/49.sh
+++ assignment/49.sh
@@ -1,3 +1,28 @@
 # Author : Mihir Mithani
 
 #!/opt/homebrew/bin/bash
+
+even() {
+    local arr=("$@")
+
+    echo "Even numbers are:"
+    for num in "${arr[@]}"
+    do
+        if (( num % 2 == 0 )); then
+            echo "$num"
+        fi
+    done
+}
+
+read -p "Enter Number of elements: " n
+
+array=()
+
+echo "Enter Elements:"
+for (( i=0; i<n; i++ ))
+do
+    read a
+    array+=("$a")
+done
+
+even "${array[@]}"
