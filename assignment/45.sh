69c6abe1a3cecdcb03f4e4d4dcf1cfe2d0b22b26
mihirmithani <mihirmithani123@gmail.com>
2026-07-08 13:33:42 +0530

    Assignment

--- assignment/45.sh
+++ assignment/45.sh
@@ -1,3 +1,10 @@
 # Author : Mihir Mithani
 
 #!/opt/homebrew/bin/bash
+
+name=$(osascript <<EOF
+text returned of (display dialog "Enter your name:" default answer "" with title "User Information")
+EOF
+)
+
+osascript -e "display dialog \"Hello, $name!\" with title \"Greeting\""
