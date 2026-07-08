c791376cef3d48786c790e7bc296e23667bc7ca4
mihirmithani <mihirmithani123@gmail.com>
2026-07-08 13:33:44 +0530

    Assignment

--- assignment/46.sh
+++ assignment/46.sh
@@ -1,3 +1,19 @@
 # Author : Mihir Mithani
 
+
 #!/opt/homebrew/bin/bash
+
+nested=(
+    "1 2 3"
+    "a b c"
+    "+ - *"
+)
+
+echo "Full nested array:"
+for group in "${nested[*]}"; do
+    echo "  $group"
+done
+
+sub_nested=${nested[1]}
+
+echo "Sub array at index 1 : ${sub_nested}"
