fa2d422908a28cd83253dd24f38c0a7bc229e17a
mihirmithani <mihirmithani123@gmail.com>
2026-07-08 10:49:36 +0530

    Assignment

--- assignment/37.sh
+++ assignment/37.sh
@@ -1,3 +1,12 @@
 # Author : Mihir Mithani
 
 #!/opt/homebrew/bin/bash
+read -p "Enter email address: " email
+
+regex='^[A-Za-z0-9._]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$'
+
+if [[ "$email" =~ $regex ]]; then
+    echo "Valid email address."
+else
+    echo "Invalid email address."
+fi
