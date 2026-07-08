c72ad3da1e24a33417abbb4488d5d57488de07e5
mihirmithani <mihirmithani123@gmail.com>
2026-07-08 13:33:46 +0530

    Assignment

--- assignment/47.sh
+++ assignment/47.sh
@@ -1,3 +1,11 @@
 # Author : Mihir Mithani
 
 #!/opt/homebrew/bin/bash
+
+numbers=(45 12 89 3 67 23 90 5)
+
+echo "Original array: ${numbers[@]}"
+
+sorted=($(printf "%d\n" "${numbers[@]}" | sort -nr))
+
+echo "Sorted (descending): ${sorted[@]}"
