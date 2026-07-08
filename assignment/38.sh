d3d7a304787b8600b43cbdc975ca6ac3f72ae341
mihirmithani <mihirmithani123@gmail.com>
2026-07-08 10:49:38 +0530

    Assignment

--- assignment/38.sh
+++ assignment/38.sh
@@ -1,3 +1,18 @@
 # Author : Mihir Mithani
 
 #!/opt/homebrew/bin/bash
+
+read -p "Enter the string : " str
+
+length=${#str}
+
+reverse=""
+
+for ((i=$length-1;i>=0;i--))
+do
+    reverse+="${str:i:1}"
+done
+
+
+echo "Original : $str"
+echo "Reverse : $reverse"
