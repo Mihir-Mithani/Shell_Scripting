cc391195f433b55714a1bf6333ed86a95eb96c24
mihirmithani <mihirmithani123@gmail.com>
2026-07-08 11:16:02 +0530

    Assignment

--- assignment/41.sh
+++ assignment/41.sh
@@ -1,3 +1,13 @@
 # Author : Mihir Mithani
 
 #!/opt/homebrew/bin/bash
+
+counts() {
+    local arr=("$@")
+    echo "Number of elements: ${#arr[@]}"
+}
+
+array=(10 20 30 40 50)
+
+echo "Array: ${array[*]}"
+counts "${array[@]}"
