4df7da7dbad17225e8bef8c3cf625edff0502b33
mihirmithani <mihirmithani123@gmail.com>
2026-07-08 10:49:28 +0530

    Assignment

--- assignment/33.sh
+++ assignment/33.sh
@@ -1,3 +1,11 @@
 # Author : Mihir Mithani
 
 #!/opt/homebrew/bin/bash
+
+read -p "Enter Year : " year
+
+if (( (year % 400 == 0) || (year % 4 == 0 && year % 100 != 0) )); then
+    echo "$year is a Leap Year."
+else
+    echo "$year is NOT a Leap Year."
+fi
