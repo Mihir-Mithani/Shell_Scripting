a8604ef28f15933328a298bfb72d1bf0bfee789b
mihirmithani <mihirmithani123@gmail.com>
2026-07-08 10:49:34 +0530

    Assignment

--- assignment/36.sh
+++ assignment/36.sh
@@ -1,3 +1,13 @@
 # Author : Mihir Mithani
 
 #!/opt/homebrew/bin/bash
+
+#!/opt/homebrew/bin/bash
+
+read -p "Enter a string: " str
+
+if [[ "$str" =~ [0-9]+ ]]; then
+    echo "Digits found: ${BASH_REMATCH[0]}"
+else
+    echo "No digits found."
+fi
