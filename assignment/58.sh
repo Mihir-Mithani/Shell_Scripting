5b3d4140c3942785ff1e342fc6db95ee8e658057
mihirmithani <mihirmithani123@gmail.com>
2026-07-10 18:02:40 +0530

    Assignment

--- assignment/58.sh
+++ assignment/58.sh
@@ -1,3 +1,18 @@
 # Author : Mihir Mithani
 
 #!/opt/homebrew/bin/bash
+
+array=(apple banana cherry date)
+
+echo "Original array: ${array[*]}"
+echo "Length: ${#array[@]}"
+
+unset 'array[1]'
+
+echo "After deleting index 1 (banana): ${array[*]}"
+echo "Length: ${#array[@]}"
+
+array=("${array[@]}")
+
+echo "After reindexing: ${array[*]}"
+echo "Length: ${#array[@]}"
