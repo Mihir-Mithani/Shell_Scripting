99a6195b71c328a651a96475615f469410f42b49
mihirmithani <mihirmithani123@gmail.com>
2026-07-10 18:02:24 +0530

    Assignment

--- assignment/50.sh
+++ assignment/50.sh
@@ -1,3 +1,11 @@
 # Author : Mihir Mithani
 
 #!/opt/homebrew/bin/bash
+
+echo "Script started at: $(date)"
+
+echo "This message goes to the log file only." >> script.log
+
+echo "This message goes to both terminal and log file." | tee -a script.log
+
+echo "Script ended at: $(date)" >> script.log
