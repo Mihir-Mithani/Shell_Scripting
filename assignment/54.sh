e3057352776b9198d55becf47f18ae92d09d08b3
mihirmithani <mihirmithani123@gmail.com>
2026-07-10 18:02:32 +0530

    Assignment

--- assignment/54.sh
+++ assignment/54.sh
@@ -1,3 +1,12 @@
 # Author : Mihir Mithani
 
 #!/opt/homebrew/bin/bash
+
+read -p "Enter first number (a): " a
+read -p "Enter second number (b): " b
+
+echo "Sum (a + b) = $((a + b))"
+echo "Difference (a - b) = $((a - b))"
+echo "Product (a * b) = $((a * b))"
+echo "Quotient (a / b) = $((a / b))"
+echo "Remainder (a % b) = $((a % b))"
