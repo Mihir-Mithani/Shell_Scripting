fbf3db58c70659d9e5805a30e0e9109844faaaed
mihirmithani <mihirmithani123@gmail.com>
2026-07-13 16:08:01 +0530

    Assignment

--- assignment/68.sh
+++ assignment/68.sh
@@ -1,3 +1,14 @@
 # Author : Mihir Mithani
 
 #!/opt/homebrew/bin/bash
+
+add() {
+    local a=$1
+    local b=$2
+    echo "Sum: $((a + b))"
+}
+
+read -p "Enter first number: " num1
+read -p "Enter second number: " num2
+
+add "$num1" "$num2"
