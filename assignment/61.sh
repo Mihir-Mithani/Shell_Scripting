b288e1b48d61a1afd4961d4b26e5a8633f78ffd2
mihirmithani <mihirmithani123@gmail.com>
2026-07-13 16:07:47 +0530

    Assignment

--- assignment/61.sh
+++ assignment/61.sh
@@ -1,3 +1,12 @@
 # Author : Mihir Mithani
 
 #!/opt/homebrew/bin/bash
+
+read -p "Enter first string: " str1
+read -p "Enter second string: " str2
+
+if [ "$str1" = "$str2" ]; then
+    echo "Strings are equal"
+else
+    echo "Strings are not equal"
+fi
