37ac40a709e81870711a6c25e6339c17adc5d96d
mihirmithani <mihirmithani123@gmail.com>
2026-07-13 16:07:53 +0530

    Assignment

--- assignment/64.sh
+++ assignment/64.sh
@@ -1,3 +1,15 @@
 # Author : Mihir Mithani
 
 #!/opt/homebrew/bin/bash
+
+read -p "Enter file path: " filepath
+
+if [ -L "$filepath" ]; then
+    echo "$filepath is a symbolic link"
+elif [ -f "$filepath" ]; then
+    echo "$filepath is a regular file"
+elif [ -d "$filepath" ]; then
+    echo "$filepath is a directory"
+else
+    echo "$filepath does not exist or is another type"
+fi
