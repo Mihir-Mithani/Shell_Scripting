e721a9499ebd540ea98435fb06b88d8e66f963d2
mihirmithani <mihirmithani123@gmail.com>
2026-07-13 16:07:49 +0530

    Assignment

--- assignment/62.sh
+++ assignment/62.sh
@@ -1,3 +1,11 @@
 # Author : Mihir Mithani
 
 #!/opt/homebrew/bin/bash
+
+day=$(date +%u)
+
+if [ "$day" -eq 6 ] || [ "$day" -eq 7 ]; then
+    echo "Today is a weekend"
+else
+    echo "Today is a weekday"
+fi
