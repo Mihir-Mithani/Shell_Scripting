58902b5403e996409def7bb473f7329c38141629
mihirmithani <mihirmithani123@gmail.com>
2026-07-13 16:08:05 +0530

    Assignment

--- assignment/70.sh
+++ assignment/70.sh
@@ -1,3 +1,33 @@
 # Author : Mihir Mithani
 
 #!/opt/homebrew/bin/bash
+
+validate_password() {
+    local password=$1
+    local errors=()
+
+    if [ ${#password} -lt 8 ]; then
+        errors+=("At least 8 characters long")
+    fi
+
+    if ! [[ "$password" =~ [0-9] ]]; then
+        errors+=("Contains at least one digit (0-9)")
+    fi
+
+    if ! [[ "$password" =~ [^a-zA-Z0-9] ]]; then
+        errors+=("Contains at least one special character")
+    fi
+
+    if [ ${#errors[@]} -eq 0 ]; then
+        echo "Password is STRONG."
+    else
+        echo "Password is WEAK. Missing:"
+        for err in "${errors[@]}"; do
+            echo "  - $err"
+        done
+    fi
+}
+
+read -s -p "Enter password: " pass
+echo
+validate_password "$pass"
