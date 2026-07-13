772294ba5f9e54c3418ed5a3ef84512c87c3c2ec
mihirmithani <mihirmithani123@gmail.com>
2026-07-13 16:07:55 +0530

    Assignment

--- assignment/65.sh
+++ assignment/65.sh
@@ -1,3 +1,19 @@
 # Author : Mihir Mithani
 
 #!/opt/homebrew/bin/bash
+
+job() {
+    local id=$1
+    local delay=$2
+    echo "Job $id started (PID: $$)"
+    sleep $delay
+    echo "Job $id finished"
+}
+
+for i in {1..5}; do
+    job $i $((RANDOM % 3 + 1)) &
+done
+
+echo "All 5 jobs started in background..."
+wait
+echo "All background jobs completed."
