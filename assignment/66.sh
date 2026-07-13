001a5184339dd3e9454f7566c75803e5b6f40d07
mihirmithani <mihirmithani123@gmail.com>
2026-07-13 16:07:57 +0530

    Assignment

--- assignment/66.sh
+++ assignment/66.sh
@@ -1,3 +1,14 @@
 # Author : Mihir Mithani
 
 #!/opt/homebrew/bin/bash
+
+echo "Creating Folder..."
+mkdir Files
+cd Files
+echo "Creating Files..."
+
+for i in {1..100}
+do
+    echo "Creating file$i.txt"
+    touch file$i.txt
+done
