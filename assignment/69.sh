d2c6f91e4105bb25375268c82843ebff95d3dcdc
mihirmithani <mihirmithani123@gmail.com>
2026-07-13 16:08:03 +0530

    Assignment

--- assignment/69.sh
+++ assignment/69.sh
@@ -1,3 +1,21 @@
 # Author : Mihir Mithani
 
 #!/opt/homebrew/bin/bash
+
+check_file() {
+    local filepath=$1
+
+    if [ -e "$filepath" ]; then
+        if [ -w "$filepath" ]; then
+            echo "File '$filepath' exists and is writable."
+        else
+            echo "File '$filepath' exists but is NOT writable."
+        fi
+    else
+        echo "File '$filepath' does NOT exist."
+    fi
+}
+
+read -p "Enter file path: " path
+
+check_file "$path"
